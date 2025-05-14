68fb1552df627064f3b84a7ac0a634db26aede0e
jose <jernestocastro01@gmail.com>
2025-05-14 16:06:37 -0400

    javascript

---
+++ src/javascript.js
@@ -0,0 +1,22 @@
+//Example of closure
+function makeFunc () {
+
+    let name = "Mozilla";
+    
+    function displayName() {
+        console.log(this.name);
+    }
+    
+    function changeName(name) {
+        name = name
+    }
+    
+  return {displayName, changeName};
+
+}
+
+const func = makeFunc();
+
+func.displayName();
+func.changeName("Godzilla");
+func.displayName();
