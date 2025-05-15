f55af7d1a281f1aed6f0eb7714417fd9b7653c52
jose <jernestocastro01@gmail.com>
2025-05-15 15:10:31 -0400

    JavaScript

---
+++ src/Exercise.js
@@ -0,0 +1,11 @@
+function mostrarVariables() {
+  if (true) {
+    var fruta = "Manzana";
+    let bebida = "Jugo";
+  }
+
+  console.log(fruta); // ¿Qué muestra?
+  console.log(bebida); // ¿Qué pasa aquí?
+}
+
+mostrarVariables();
