411d8bbc7bbb05adf53209147b955da701f3609e
jose <jernestocastro01@gmail.com>
2025-07-11 19:34:25 -0400

    Promises, use of then catch y finally 2

--- examplePromise.js
+++ examplePromise.js
@@ -10,3 +10,31 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto")
     console.log("Request completed");
   });
 
+//second Example
+function tareaSimple() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    setTimeout(() => {
+      if (success) {
+        resolve("Task has been completed without issues");
+      } else {
+        reject("Have an issue occured");
+      }
+    }, 1000);
+  });
+}
+
+tareaSimple()
+  .then((mensaje) => {
+    console.log("THEN 1:", mensaje);
+    return "Result processed in THEN 2";
+  })
+  .then((nuevoMensaje) => {
+    console.log("THEN 2:", nuevoMensaje);
+  })
+  .catch((error) => {
+    console.log("CATCH:", error);
+  })
+  .finally(() => {
+    console.log("FINALLY: always working");
+  });
